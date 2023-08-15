d3cbc16a12fa1ad4ed78ee1771ce998572666e1e
Monica Calderaro <mcalderarorey@gmail.com>
2023-08-15 12:06:33 +0200

    add initial structure for the app

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import { TVShowAPI } from "./api/tv-show";
+import { BACKDROP_BASE_URL } from "./config";
+
+export function App() {
+  const [currentTVShow, setCurrentTVShow] = useState();
+
+  async function fetchPopulars() {
+    const popularTVShowList = await TVShowAPI.fetchPopulars();
+    if (popularTVShowList.length > 0) {
+      setCurrentTVShow(popularTVShowList[0]);
+    }
+  }
+  useEffect(() => {
+    fetchPopulars();
+  }, []);
+  console.log(currentTVShow);
+  return (
+    <div
+      className="main-container"
+      style={{
+        background: currentTVShow
+          ? `linear-gradient(rgba(0, 0, 0, 0.55), rgba(0, 0, 0, 0.55)),
+             url("${BACKDROP_BASE_URL}${currentTVShow.backdrop_path}") no-repeat center / cover`
+          : "black",
+      }}
+    >
+      <div className="header">
+        search bar
+        <div className="row">
+          <div className="col-4">
+            <div>LOGO</div>
+            <div>Subtitle</div>
+          </div>
+          <div className="col-md-12 col-lg-4">
+            <input style={{ width: "100%" }} type="text"></input>
+          </div>
+        </div>
+      </div>
+      <div className="tv-show-detail">movie</div>
+      <div className="recommendations">recommendations</div>
+    </div>
+  );
+}
