8a48b71d9d83889529652b3e00880e985e87eaee
Monica Calderaro <mcalderarorey@gmail.com>
2023-08-18 11:11:44 +0200

    clean input after submit

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,11 +1,19 @@
 import style from "./style.module.css";
+import { useState } from "react";
 import { Search as SearchIcon } from "react-bootstrap-icons";
 
 export const SearchBar = ({ onSubmit }) => {
+  const [inputValue, setInputValue] = useState("");
+
   const handleEnter = (e) => {
     if (e.key === "Enter" && e.target.value.trim() !== "") {
       onSubmit(e.target.value);
+      setInputValue("");
     }
+  };
+
+  const handleChange = (e) => {
+    setInputValue(e.target.value);
   };
   return (
     <>
@@ -14,7 +22,9 @@ export const SearchBar = ({ onSubmit }) => {
         className={style.input}
         type="text"
         placeholder="Search a tv show"
+        onChange={handleChange}
         onKeyUp={handleEnter}
+        value={inputValue}
       />
     </>
   );
